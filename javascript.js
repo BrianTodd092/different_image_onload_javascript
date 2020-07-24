37f893c265c76bc1b3e3e274b157fa6dc5a792b1
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-07-24 19:41:23 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,13 +12,15 @@ var greeting;
  else if (d > 12) {
      greeting = document.getElementById('b').style.display = 'block';
 }
- else if (d > 0) {
+ else if ((d > 0) && (d < 7)){
      greeting = document.getElementById('c').style.display = 'block';
 }
  
- else {
+ else if ( d > 6) {
        greeting = document.getElementById('d').style.display = 'block';
 }
+
+else { greeting=""; }
 
 
 document.image = greeting;
